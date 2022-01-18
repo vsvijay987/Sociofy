31a94333afefd9f2468ae407b5be95a14a63f427
vsvijay987 <vsvijju987@gmail.com>
2022-01-18 20:22:51 +0530

    deployment ready

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl ="http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://sociofy-v1.herokuapp.com/";
 
 module.exports = baseUrl;
